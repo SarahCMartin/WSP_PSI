Adding more dps to roughness-depth correction interpolation formula to fix errors at small D

Common.py
@@ -221,8 +221,10 @@ def generate_rolls(d, No_rolls, results_path=None):
         elif dist == 'normal':
             mu, sigma = Distributions.fit_normal_to_percentiles(LE, BE, HE, Min)
             fit_info[name] = [dist, (mu, sigma)]
-            rolls[name] = truncnorm.rvs(loc=mu, scale=sigma, size=No_rolls)
-            Distributions.plot_distribution_fit([LE, BE, HE], [0.05, 0.5, 0.95], truncnorm, (mu, sigma), samples=rolls[name], param_name=name, dist_name='Normal', results_path=results_path, type='input')
+            a = (Min - mu)/sigma
+            b = np.inf
+            rolls[name] = truncnorm.rvs(a, b, loc=mu, scale=sigma, size=No_rolls)
+            Distributions.plot_distribution_fit([LE, BE, HE], [0.05, 0.5, 0.95], truncnorm, (a, b, mu, sigma), samples=rolls[name], param_name=name, dist_name='Normal', results_path=results_path, type='input')
 
         elif dist == 'log-normal':
             s, loc, scale = Distributions.fit_lognormal_to_percentiles(LE, BE, HE, Min)

PSI_correlate.py
@@ -131,13 +131,22 @@ def induce_correlations(data, corrmat):
 
     # Sort the original data according to new_data_rank
     for i in range(data.shape[1]):
-        vals, order = np.unique(
-            np.hstack((data_rank[:, i], new_data_rank[:, i])), return_inverse=True
-        )
-        old_order = order[: new_data_rank.shape[0]]
-        new_order = order[-new_data_rank.shape[0] :]
-        tmp = data[np.argsort(old_order), i][new_order]
-        data[:, i] = tmp[:]
+        # Sort the original column by its rank
+        sorted_data = np.sort(data[:, i])
+    
+        # Get the order that sorts the new ranks
+        new_order = np.argsort(np.argsort(new_data_rank[:, i]))
+    
+        # Reassign the sorted values according to the new rank order
+        data[:, i] = sorted_data[new_order]
+        
+        # vals, order = np.unique(
+        #     np.hstack((data_rank[:, i], new_data_rank[:, i])), return_inverse=True
+        # )
+        # old_order = order[: new_data_rank.shape[0]]
+        # new_order = order[-new_data_rank.shape[0] :]
+        # tmp = data[np.argsort(old_order), i][new_order]
+        # data[:, i] = tmp[:]
 
     return data
 